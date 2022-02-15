d88dbceab749bf8eed99d821c3fe606a4cc77c50
ColdX <54853042+ColdXII@users.noreply.github.com>
2022-02-15 10:25:54 +0700

    latihan 1

---
+++ Latihan 1/break_Continue.kt
@@ -0,0 +1,34 @@
+fun main()
+{
+    var index:Int = 1
+
+    var students = arrayOf("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+
+    for (student in students)
+    {
+        if (student == "d") {
+            continue
+//        }else if{
+//            break
+        }
+
+
+        println(student)
+//        continue
+
+    }
+
+//    while (index <= 10)
+//    {
+//        if (index == 5)
+//        {
+//            index++ // continue
+////            println(index)
+//            continue
+//            break
+//        }
+//
+//        println(index)
+//        index++
+//    }
+}
